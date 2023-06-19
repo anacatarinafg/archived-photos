82c8947b6c55e24b03ad2cc033dcfe109c08233b
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-06-19 15:16:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let images = document.querySelectorAll(".gallery__image");
+let prevX = 0;
+let prevY = 0;
+let moveXAmount = 0;
+let moveYAmount = 0;
+
+document.addEventListener("mousemove", function (e) {
+    cursorPosition(e)
+})
+
+function cursorPosition(e) {
+    moveXAmount = e.pageX - prevX;
+    moveYAmount = e.pageY - prevY;
+
+    moveImage(moveXAmount, moveYAmount);
+
+    prevX = e.pageX;
+    prevY = e.pageY;
+}
+
+function moveImage(xAmount, yAmount) {
+    images.forEach(function (image) {
+        let movementStrength = 5 + (Math.random() * 15);
+
+        image.style.left = (image.offsetLeft) - (xAmount / movementStrength) + "px";
+        image.style.top = (image.offsetTop) - (yAmount / movementStrength) + "px";
+
+
+
+    })
+}
